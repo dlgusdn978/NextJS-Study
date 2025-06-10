c23e3cbdd52b97c7f3185ade678bd9396f76b48a
dlgusdn978 <dlgusdn978@naver.com>
2025-06-10 21:15:46 +0900

    Feat(FE): pagination 적용

--- restaurant-app/src/pages/stores/index.tsx
+++ restaurant-app/src/pages/stores/index.tsx
@@ -1,16 +1,23 @@
-import { StoreType } from "@/interface";
+import { StoreApiResponse } from "@/interface";
 import Image from "next/image";
 import axios from "axios";
 import { useQuery } from "react-query";
 import Loading from "@/component/Loading";
+import Link from "next/link";
+
+import { useRouter } from "next/router";
+import Pagination from "@/component/Pagination";
 export default function StoreListPage() {
+  const router = useRouter();
+  const { page = "1" }: any = router.query;
+  console.log(page);
   const {
     isLoading,
     isError,
     data: stores,
-  } = useQuery("stores", async () => {
-    const { data } = await axios("/api/stores");
-    return data as StoreType[];
+  } = useQuery(`stores/${page}`, async () => {
+    const { data } = await axios(`/api/stores?page=${page}`);
+    return data as StoreApiResponse;
   });
 
   if (isError) {
@@ -27,7 +34,7 @@ export default function StoreListPage() {
         {isLoading ? (
           <Loading />
         ) : (
-          stores?.map((store, index) => (
+          stores?.data.map((store, index) => (
             <li className="flex justify-between gap-x-6 py-5" key={index}>
               <div className="flex gap-x-4">
                 <Image
@@ -63,6 +70,10 @@ export default function StoreListPage() {
           ))
         )}
       </ul>
+      {stores?.totalPage && (
+        <Pagination total={stores?.totalPage} page={page} />
+      )}
+
       <h1>Store List</h1>
     </div>
   );
